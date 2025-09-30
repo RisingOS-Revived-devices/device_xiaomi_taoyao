feecf270286cb57930322bdd2e54ba21c7b37b75
Ammar Bahtiar <ammarbahtiarasli@gmail.com>
2025-09-30 08:47:02 +0700

    taoyao: Initialize for RisingOS-Revived 8.++

--- lineage_taoyao.mk
+++ lineage_taoyao.mk
@@ -14,7 +14,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_INCLUDE_ACCORD := false
 TARGET_DISABLE_EPPE := true
-WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_taoyao
@@ -31,7 +30,20 @@ SystemDevice := taoyao
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="taoyao_global-user 14 UKQ1.231003.002 V816.0.26.0.ULIMIXM release-keys" \
     DeviceName=$(SystemDevice) \
-    DeviceProduct=$(SystemName)
+    DeviceProduct=$(SystemName) \
+    RisingChipset="Snapdragon 778G" \
+    RisingMaintainer="ammarbahtiarasli"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/taoyao_global/taoyao:14/UKQ1.231003.002/V816.0.26.0.ULIMIXM:user/release-keys
+
+# Lunch banner maintainer variable
+RISING_MAINTAINER="ammarbahtiarasli"
+RISING_MAINTAINER := ammarbahtiarasli
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# GMS build flags, true by default
+# Ship with GMS packages, replaces default AOSP packages with Google manufactured packages.
+WITH_GMS := true
